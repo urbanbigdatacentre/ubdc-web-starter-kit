f6a62263e1869e81d47d041d0a7090c444326fa8
andyclarkemedia <andyclarkemedia@gmail.com>
2023-04-27 10:45:56 +0100

    Config Files - GH Actions - Dockerfile - License - Yarn - next.config.js - fonts - tsconfig.json

---
+++ styles/theme.ts
@@ -0,0 +1,13 @@
+// Custom theme for the app
+
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1a237e',
+        },
+    }
+});
+
+export default theme;
