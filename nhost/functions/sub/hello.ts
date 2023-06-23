e16744362e8ddf057ff7b438b13cfe92ed9caef3
andyclarkemedia <andyclarkemedia@gmail.com>
2023-06-23 11:39:56 +0100

    Initial Config of Docs Branch

--- nhost/functions/sub/hello.ts
+++
@@ -1,6 +0,0 @@
-import {NextApiRequest, NextApiResponse} from "next";
-
-export default (req: NextApiRequest,
-                res: NextApiResponse) => {
-  res.status(200).send(`Hello from a subdirectory, ${req.query.name}!`)
-}
--- nhost/functions/sub/index.ts
+++
@@ -1,6 +0,0 @@
-import {NextApiRequest, NextApiResponse} from "next";
-
-export default (req: NextApiRequest,
-                res: NextApiResponse) => {
-  res.status(200).send(`Index function in a sub-directory`)
-}
