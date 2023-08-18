1fe9bccf5b4e8718e6eecd545a12d5b410a82bd9
andyclarkemedia <andyclarkemedia@gmail.com>
2023-08-18 13:53:25 +0100

    Removed getDocData function as unneccessary

--- lib/docs.ts
+++ lib/docs.ts
@@ -1,8 +1,6 @@
 // Get All Document Files Utility Function
 
-import * as path from "path";
 import * as fs from 'fs';
-import parseMarkdown from "@/utils/parseMarkdown";
 
 
 export function getFileStructure() {
@@ -67,27 +65,4 @@ export function getAllDocPaths() {
             },
         };
     });
-}
-
-export async function getDocData(id:  Array<string> | undefined) {
-    const docsDirectory = 'docs';
-    const matter = require('gray-matter');
-
-    const fullPath = path.join(docsDirectory, `${id?.join('/') }.md`);
-    const fileContents = fs.readFileSync(fullPath, 'utf8');
-    const docsDirectoryContents = fs.readdirSync(docsDirectory);
-
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
-    // Use remark to convert markdown into HTML string
-    const contentHtml = await parseMarkdown(matterResult.content);
-
-    // Combine the data
-    return {
-        id,
-        contentHtml,
-        fullPath,
-        docsDirectoryContents,
-        ...matterResult.data,
-    };
 }
--- pages/docs/[...id].tsx
+++ pages/docs/[...id].tsx
@@ -1,5 +1,5 @@
 // Component to render a single document from markdown files located in root/docs
-import {getAllDocPaths, getAllDocs, getDocData, getFileStructure} from "@/lib/docs";
+import {getAllDocPaths, getAllDocs, getFileStructure} from "@/lib/docs";
 import {ParsedUrlQuery} from "querystring";
 import StandardContainer from "@/components/layouts/StandardContainer";
 import BasePageComponents from "@/components/layouts/BasePageComponents";
@@ -26,34 +26,33 @@ import rehypeMermaid from "rehype-mermaidjs";
 
 
 interface DocProps {
-    source: MDXRemoteSerializeResult
-    docData: {
-        id: string;
-        fullPath: string;
-        title: string;
-        author: string;
-        tags: string[];
-        contentHtml: string;
-        docsDirectoryContents: string | string[];
-    }
+    source:
+        {
+            compiledSource: MDXRemoteSerializeResult,
+            frontmatter: {
+                title: string;
+                author: string;
+                category: string;
+                tags: string[];
+            }
+        }
     docFileStructure: Object;
 }
 
 
 const Doc = (props : DocProps) => {
 
-    const doc = props.docData;
     const fileStructure = props.docFileStructure;
     const theme = useTheme();
     const router = useRouter();
-
+    const frontMatter = props.source.frontmatter;
 
     // @ts-ignore
     // @ts-ignore
     return (
         <BasePageComponents>
             <Head>
-                <title>{doc.title}</title>
+                <title>{frontMatter.title}</title>
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="initial-scale=1, width=device-width" />
                 <meta name="theme-color" content="#000000" />
@@ -68,16 +67,16 @@ const Doc = (props : DocProps) => {
                     <Box sx={{backgroundColor: theme.palette.action.hover, border: `1px dashed ${theme.palette.action.active}`, width: `max-content`, paddingLeft: theme.spacing(1), paddingRight: theme.spacing(1)}}>
                         <Typography variant={'body2'} sx={{color: theme.palette.action.active, fontWeight: 500, textTransform: `capitalize`}}>{router.asPath.split('/')[2].split('#')[0]}</Typography>
                     </Box>
-                    <Typography variant={'h1'}>{doc.title}</Typography>
+                    <Typography variant={'h1'}>{frontMatter.title}</Typography>
                     <Divider component="div" sx={{width: `100%`}}/>
                     <Stack sx={{gap: theme.spacing(2)}}>
                         <Box sx={{display: `flex`, width: `100%`, justifyContent: `space-between`, flexWrap: `wrap`, gap: theme.spacing(1) }}>
-                            <Typography variant={'caption'} sx={{color: theme.palette.grey[600]}}>{doc.fullPath.replace('.md', '')}</Typography>
-                            <Typography variant={'caption'} sx={{color: theme.palette.grey[900]}}>{doc.author}</Typography>
+                            <Typography variant={'caption'} sx={{color: theme.palette.grey[600]}}>{window.location.href.split('/').slice(3).join('/')}</Typography>
+                            <Typography variant={'caption'} sx={{color: theme.palette.grey[900]}}>{frontMatter.author}</Typography>
                         </Box>
                         <Box sx={{display: `flex`, gap: theme.spacing(1)}}>
                             {
-                                doc?.tags?.map((tag, index) => {
+                                frontMatter?.tags?.map((tag, index) => {
                                     return <TagBlock text={tag} key={index}/>
                                 })
                             }
@@ -110,7 +109,7 @@ interface Params extends ParsedUrlQuery {
 
 export async function getStaticProps({params}: Params) {
     if (params != null) {
-        const docData = await getDocData(params['id']);
+
         const docFileStructure = await getFileStructure();
         const docPaths = await getAllDocPaths();
         const source = fs.readFileSync(`docs/${params['id'].join('/')}.md`)
@@ -124,7 +123,6 @@ export async function getStaticProps({params}: Params) {
         return {
             props: {
                 source: mdxSource,
-                docData,
                 docPaths,
                 docFileStructure,
             },
